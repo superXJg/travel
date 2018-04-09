61b9b2f019b2bce75780af5a253deca76c2999c4
xianyu <15659355035@163.com>
2018-04-09 22:51:44 +0800

    change

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,18 @@
 <template>
    <div>
      <m-head></m-head>
+     <m-swiper></m-swiper>
    </div>
 </template>
 
 <script>
 import mHead from '@/pages/home/m-head'
+import mSwiper from '@/pages/home/m-swiper'
 export default {
   name: 'home',
   components: {
-    mHead
+    mHead,
+    mSwiper
   }
 }
 </script>
---
+++ src/pages/home/m-swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-swiper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperOption.swiperSlides" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  /* eslint-disable */
+  name: 'm-swiper',
+  data () {
+    return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          swiperSlides: [
+            {
+              id: 100,
+              imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/23/da45ee3852538002.jpg_640x200_a17ebc59.jpg'
+            },
+            {
+              id: 101,
+              imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/81/61e16fc667c0bb02.jpg_640x200_ea28d62a.jpg'
+            },
+          ],
+          loop: true
+        }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.home-swiper /deep/ .swiper-pagination-bullet-active{
+  background-color: #fff;
+}
+.item-img{
+  width: 100%;
+}
+</style>
