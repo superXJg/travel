13d8dba6b6a92ec8d462c6603dc59cdb81eddcd5
旗木卡卡西 <lx@163.com>
2018-04-09 17:27:55 +0800

    home component

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+     <m-head></m-head>
+   </div>
+</template>
+
+<script>
+import mHead from '@/pages/home/m-head'
+export default {
+  name: 'home',
+  components: {
+    mHead
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home/m-head.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='head'>
+    <div class="head-left">返回</div>
+    <div class="head-input">
+    </div>
+    <div class="head-right">北京</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'm-head'
+}
+</script>
+
+<style scoped lang='less'>
+@import "../../assets/css/varibles.less";
+.head{
+  height: 0.86rem;
+  line-height: 0.86rem;
+  display: flex;
+  background-color: @bgColor;
+  color: #fff;
+  .head-left{
+    width: .64rem;
+  }
+  .head-right{
+    width: 1.24rem;
+    text-align: center;
+  }
+  .head-input{
+    flex: 1;
+    background-color: #fff;
+    height: 0.64rem;
+    margin-top: 0.12rem;
+    border-radius: 5px;
+  }
+}
+</style>
