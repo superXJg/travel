b04fdec134f6284913643d59a74614bd5103c374
旗木卡卡西 <lx@163.com>
2018-04-18 10:16:32 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/home'
-import City from '@/pages/city/city'
-import Detail from '@/pages/detail/detail'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      component: Home
+      component: () => import('@/pages/home/home')
     },
     {
       path: '/city',
-      component: City
+      component: () => import('@/pages/city/city')
     },
     {
       path: '/detail/:id',
-      component: Detail
+      component: () => import('@/pages/detail/detail')
     }
   ]
 })
